ba11a5a0cbc73a8c5f3a3878c16d3bacf00c64fb
Hin Chi WONG <phantomcase@gmail.com>
2021-12-09 02:14:54 +0000

    function complete

--- src/main/java/com/techreturners/exercise001/Exercise001.java
+++ src/main/java/com/techreturners/exercise001/Exercise001.java
@@ -1,30 +1,48 @@
 package com.techreturners.exercise001;
 
 import java.util.List;
+import java.text.DecimalFormat;
 
 public class Exercise001 {
     public String capitalizeWord(String word) {
         // Add your code here
-        return "";
+        String resultStr = word.substring(0, 1).toUpperCase() + word.substring(1);
+        return resultStr;
     }
 
     public String generateInitials(String firstName, String lastName) {
         // Add your code here
-        return "";
+        String resultStr = firstName.substring(0, 1).toUpperCase() + "." + lastName.substring(0, 1).toUpperCase();
+        return resultStr;
     }
 
     public double addVat(double originalPrice, double vatRate) {
         // Add your code here
-        return 0.0;
+        double result = originalPrice+(originalPrice * vatRate * 0.01);
+        DecimalFormat df = new DecimalFormat("#.##");
+        return  Double.valueOf(df.format(result));
     }
 
     public String reverse(String sentence) {
         // Add your code here
-        return "";
+
+        if (sentence == null) {
+            return null;
+        }
+
+        String result = "";
+
+        for (int i = sentence.length() - 1; i >= 0; i--) {
+            result = result + sentence.charAt(i);
+        }
+
+        return result;
     }
 
     public int countLinuxUsers(List<User> users) {
         // Add your code here
-        return 0;
+        long nofLinuxUsers = users.stream().filter(user -> user.getType().equals("Linux") ).count();
+
+        return (int) nofLinuxUsers;
     }
 }
