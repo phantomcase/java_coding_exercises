9c74326bcd5e9d5dbdd9fef415f40cf198518593
Hin Chi WONG <phantomcase@gmail.com>
2021-12-10 10:47:40 +0000

    Exercise003 complete

--- src/main/java/com/techreturners/exercise003/Exercise003.java
+++ src/main/java/com/techreturners/exercise003/Exercise003.java
@@ -1,13 +1,16 @@
 package com.techreturners.exercise003;
 
+import java.util.Arrays;
+
 public class Exercise003 {
+    final String[] flavoursMap = new String[] { "Pistachio", "Raspberry Ripple", "Vanilla", "Mint Chocolate Chip", "Chocolate", "Mango Sorbet" };
 
     int getIceCreamCode(String iceCreamFlavour) {
-        throw new UnsupportedOperationException(("You can delete this statement and add your code here."));
+        return Arrays.asList(flavoursMap).indexOf(iceCreamFlavour);
     }
 
     String[] iceCreamFlavours() {
-        throw new UnsupportedOperationException(("You can delete this statement and add your code here."));
+        return flavoursMap;
     }
 
 }
