26bd87fbe3c32f98e5f60d6b0a855bb32537df40
Hin Chi WONG <phantomcase@gmail.com>
2021-12-11 22:42:26 +0000

    Exercise005 Complete

--- src/main/java/com/techreturners/exercise005/Exercise005.java
+++ src/main/java/com/techreturners/exercise005/Exercise005.java
@@ -1,9 +1,16 @@
 package com.techreturners.exercise005;
 
+import java.util.regex.MatchResult;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Exercise005 {
 
     public boolean isPangram(String input) {
-        throw new UnsupportedOperationException("You can delete this statement and add your code here.");
+        String trimInput = input.toLowerCase().replaceAll("(.)(?=.*\\1)","");
+        trimInput = trimInput.replaceAll("[^a-z]+","");
+        Integer nofChar = trimInput.length();
+        return nofChar == 26;
     }
 
 }
